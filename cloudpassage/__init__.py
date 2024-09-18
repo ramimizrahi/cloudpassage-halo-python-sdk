a75b661b1d108554b89f8a1a01026e179261d4b9
ramimizrahi <rami.mizrahi@fidelissecurity.com>
2024-09-18 12:26:39 +0300

    version 1.6.5

--- cloudpassage/__init__.py
+++ cloudpassage/__init__.py
@@ -56,5 +56,5 @@ if init_util.verify_python_version(installed, minimum) is False:
     print(err_msg)
 
 __author__ = "CloudPassage"
-__version__ = "1.6.4"
+__version__ = "1.6.5"
 __license__ = "BSD"
